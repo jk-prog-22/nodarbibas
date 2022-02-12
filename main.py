83c5e1b93df2ba1ec9592090972eec8b07abdc80
Maris Danne <maris.danne@gmail.com>
2022-02-12 11:15:15 +0200

    pirma dala

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Sveika, pasaule!")
+print("Šodien ir forša diena!")
+print()
+
+print("piens kefirs sviests biezpiens krejums")
+print("piens kefirs","sviests","biezpiens","krejums")
+print("piens kefirs","sviests","biezpiens krejums", sep = "*")
+
+# Uzskaitījums
+# TODO: Pabeigt rakstīt dzeju
+'''
+Šis ir garš komentārs
+Pa vairākām
+rindiņām
+'''
+print("berzs, ozols, osis, egle", end = "=====") # Šis ir komentārs
+print("lacis, \t kakis,\n suns, zurka") # Vēl viens komentārs
+
+print("Gribu ātrāk siltu laiku.")
+print("Gribu ātrāk siltu laiku.")
+print("Gribu ātrāk siltu laiku.")
+# print("Gribu ātrāk siltu laiku.") Aizkomentē ar Ctrl + /
+# print("Gribu ātrāk siltu laiku.")
+# print("Gribu ātrāk siltu laiku.")
+# print("Gribu ātrāk siltu laiku.")
+print("Gribu ātrāk siltu laiku.")
+print("Gribu ātrāk siltu laiku.")
---
+++ uzdevumi_print.md
@@ -0,0 +1,15 @@
+# Komandas print() uzdevumi
+## 1. uzdevums
+Izdrukāt savu vārdu.
+
+## 2. uzdevums
+Izdrukāt dzejoļa/dziesmas četrrindi, tā, lai katra rindiņa būtu ar 2 atstarpēm tālāk (no kreisās puses), kā iepriekšējā.
+
+## 3. uzdeuvms
+Izdrukāt to pašu četrrindi izmantojot vienu print komandu.
+
+## 4. uzdevums
+Izdrukāt eglīti ar zvaigznītēm.
+
+## 5. uzdevums
+Izdrukāt divus trīsstūrus viens otram blakus (augstumā 3)
