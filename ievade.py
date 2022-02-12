2927bb46bee794675f67a124c297ed7089c5fac5
Maris Danne <maris.danne@gmail.com>
2022-02-12 14:43:51 +0200

    ievade

---
+++ ievade.py
@@ -0,0 +1,11 @@
+# ievade = input()
+# print("Tika ievadīts", ievade)
+
+sk1 = int(input("Ievadi skaitli 1:"))
+sk2 = int(input("Ievadi skaitli 2:"))
+
+print(type(sk1))
+print(type(sk2))
+
+print(sk1 + sk2)
+
---
+++ uzdevumi_ievade.md
@@ -0,0 +1,4 @@
+# Ievades uzdevumi
+## 1. uzdevums
+Izveidot dinamisku ievadi, iepriekš izstrādātajiem uzdevumiem.
+uzdevumi_mainigie.md un uzdevumi_matematika.md
