672723bd4587622721e13daf187edfd9ad7f91fa
Maris Danne <maris.danne@gmail.com>
2022-03-19 15:57:07 +0200

    ML

--- 4_diena/web_scrape_ml/skrape_ss.py
+++ 4_diena/web_scrape_ml/skrape_ss.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
+import time
 
-URL = "https://www.ss.lv/lv/transport/cars/today/sell/"
+# https://www.ss.lv/lv/transport/cars/today/sell/filter/page2.html
+URL = "https://www.ss.lv/lv/transport/cars/today/"
+LAPAS = "lapas/"
+DATI = "dati/"
 
-def saglabat_lapu():
-    pieprasijums = requests.get(URL)
+def saglabat_lapu(adrese, datne):
+    pieprasijums = requests.get(adrese)
 
     if pieprasijums.status_code == 200:
         lapa = pieprasijums.text
@@ -13,13 +17,12 @@ def saglabat_lapu():
         print("Radās kļūda lapas pieprasīšanā!")
         exit()
     
-    with open("lapas/lapa_1.html", "w", encoding="UTF-8") as f:
+    with open(datne, "w", encoding="UTF-8") as f:
         f.write(lapa)
 
-# saglabat_lapu()
 
-def info():
-    with open("lapas/lapa_1.html", "r", encoding="UTF-8") as f:
+def info(htmlDatne):
+    with open(htmlDatne, "r", encoding="UTF-8") as f:
         html = f.read()
 
     zupa = bs(html, "html.parser")
@@ -69,7 +72,7 @@ def info():
         else:
             continue # ja nav nobraukums ignorējam
 
-        auto["cena"] = lauki[7].text.replace(",", "").replace("  €", "")
+        auto["cena"] = lauki[7].text.replace(",", "").replace("  €", "").replace("maiņai", "")
 
         
         if lauki[3].br:
@@ -87,4 +90,35 @@ def info():
     return dati
 
 
-info()
+#auto_dati = info()
+
+def saglabatDatus(auto_dati):
+    with open("dati/ss_auto.csv", "w", encoding="UTF-8", newline="") as f:
+        kolonas = ['razotajs', 'modelis', 'marka', 'gads', 'dzinejs', 'tilpums', 'nobraukums', 'cena']
+        w = csv.DictWriter(f, fieldnames=kolonas)
+        w.writeheader()
+        for auto in auto_dati:
+            w.writerow(auto)
+
+#saglabatDatus(auto_dati)
+
+def atvilkt_lapas(daudzums):
+    for i in range(1, daudzums + 1):
+        adrese = f"{URL}page{i}.html"
+        datne = f"{LAPAS}lapa_{i}.html"
+        print("Pieprasījums uz", adrese, "-- uz -->", datne)
+        saglabat_lapu(adrese, datne)
+        time.sleep(2)
+
+def izvelkam_datus(daudzums):
+    visiDati = []
+    for i in range(1, daudzums + 1):
+        datne = f"{LAPAS}lapa_{i}.html"
+        datnesDati = info(datne)
+        visiDati += datnesDati # visiDati = visiDati + datnesDati
+
+    saglabatDatus(visiDati)
+
+
+#atvilkt_lapas(25)
+izvelkam_datus(25)
---
+++ 4_diena/web_scrape_ml/vizualizacija.py
@@ -0,0 +1,59 @@
+import pandas as pd # datu apstrāde
+from termcolor import colored as cl # teksta izvade
+import matplotlib.pyplot as plt # vizualizācija
+import seaborn as sb # vizualizācija
+
+# vizualizaciju pamata konfigurācija
+sb.set_style('whitegrid') # plot style
+plt.rcParams['figure.figsize'] = (15, 10) # plot size
+
+
+# Karstuma karte (korelācija)
+def karstuma_karte(datne, saglabat=False):
+    df = pd.read_csv(datne)
+    sb.heatmap(df.corr(method = 'spearman'), annot = True, cmap = 'coolwarm')
+    if saglabat:
+        # izveidojam datnes nosaukumu bez mapes un faila tipe
+        datnes_vards = datne[datne.find("/"):datne.find(".")]
+        plt.savefig('atteli/{}-heatmap.png'.format(datnes_vards))
+    plt.show()
+
+
+# Lieluma sastopamības biežums
+def sadalijuma_grafiks(datne, kolonna, saglabat=False):
+    df = pd.read_csv(datne)
+    sb.distplot(df[kolonna], color = 'r')
+    plt.title(kolonna.capitalize() + ' biežums', fontsize = 16)
+    plt.xlabel(kolonna.capitalize(), fontsize = 14)
+    plt.ylabel('Biežums', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    if saglabat:
+        plt.savefig('atteli/{}.png'.format(kolonna))
+    plt.show()
+
+
+# Izkliedes grafiks (scatter plot)
+def izkliedes_grafiks(datne, x, y, saglabat=False):
+    df = pd.read_csv(datne)
+    i = df.columns
+    
+    plot1 = sb.scatterplot(x, y, data = df, color = 'orange', edgecolor = 'b', s = 15)
+    plt.title('{} / {}'.format(x, y), fontsize = 16)
+    plt.xlabel('{}'.format(x), fontsize = 14)
+    plt.ylabel('{}'.format(y), fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    if saglabat:
+        plt.savefig('atteli/{}-{}.png'.format(x, y))
+    plt.show()
+
+
+datne1 = 'dati/ss_auto.csv'
+
+# karstuma_karte(datne1, True)
+# sadalijuma_grafiks(datne1, "nobraukums")
+# sadalijuma_grafiks(datne1, "cena")
+# izkliedes_grafiks(datne1, 'gads', 'cena')
+# izkliedes_grafiks(datne1, 'cena', 'nobraukums')
+# izkliedes_grafiks(datne1, 'gads', 'nobraukums')
